d468362c35c8eb7c248de6fe412aea8bfe022fc2
lucianomores <luciano.mores@softplan.com.br>
2015-09-14 17:57:36 -0300

    Increse quiet time, new readme.md

--- README.md
+++ README.md
@@ -3,13 +3,24 @@ periodic-data-backup
 
 Docker container that monitor and backup a folder to S3 using [duplicity](http://duplicity.nongnu.org/) to backup and [inotify-tools ](http://linux.die.net/man/7/inotify) to monitor files changes.
 
-Use
----
+### Use
+
 ```shell
-docker run -e S3_URL=s3://foo.bar -e AWS_ACCESS_KEY_ID=foo -e AWS_SECRET_ACCESS_KEY=bar -e PATH_BACKUP=/my/folder/ leslau/periodic-data-backup
+docker run [OPTIONS] leslau/periodic-data-backup
 ```
 
-- S3_URL = Your bucket url ex: s3://s3-sa-east-1.amazonaws.com/foo-bar
-- AWS_ACCESS_KEY_ID = Your access key id to S3
-- AWS_SECRET_ACCESS_KEY = Your secret access key to S3
-- PATH_BACKUP = Folder that you want to monitor and backup.
+### Parameters:
+
+* `S3_URL`: Your bucket url ex: s3://s3-sa-east-1.amazonaws.com/foo-bar
+* `AWS_ACCESS_KEY_ID`: Your access key id to S3.
+* `AWS_SECRET_ACCESS_KEY`: Your secret access key to S3.
+* `PATH_TO_BACKUP`: Folder that you want to monitor and backup.
+
+### Examples:
+
+    docker run -d \
+        -e AWS_ACCESS_KEY_ID=foo \
+        -e AWS_SECRET_ACCESS_KEY=bar \
+        -e S3_URL=s3://s3-sa-east-1.amazonaws.com/foo-bar \
+        -e PATH_TO_BACKUP=/my/folder/to/backup \
+        leslau/periodic-data-backup
--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ VARIABLES=(
   S3_URL
   AWS_ACCESS_KEY_ID
   AWS_SECRET_ACCESS_KEY
-  PATH_BACKUP
+  PATH_TO_BACKUP
 )
 
 for var in "${VARIABLES[@]}"; do
@@ -27,22 +27,22 @@ ntpdate pool.ntp.org
 
 inotifywait_events="modify,attrib,move,create,delete"
 
-echo "Starting recovery from $S3_URL to $PATH_BACKUP"
-duplicity --progress --progress-rate 5 --force --no-encryption $S3_URL $PATH_BACKUP
+echo "Starting recovery from $S3_URL to $PATH_TO_BACKUP"
+duplicity --progress --progress-rate 5 --force --no-encryption $S3_URL $PATH_TO_BACKUP
 echo "Recovery complete!"
 
 # Now, start waiting for file system events on this path.
 # After an event, wait for a quiet period of N seconds before doing a backup
 
-echo "Starting monitoring $PATH_BACKUP"
-while inotifywait -r -e $inotifywait_events $PATH_BACKUP ; do
+echo "Starting monitoring $PATH_TO_BACKUP"
+while inotifywait -r -e $inotifywait_events $PATH_TO_BACKUP ; do
   echo "Change detected."
-  while inotifywait -r -t $WAIT_TIME -e $inotifywait_events $PATH_BACKUP ; do
+  while inotifywait -r -t $WAIT_TIME -e $inotifywait_events $PATH_TO_BACKUP ; do
   	echo "waiting for quiet period.."
   done
 
   echo "Starting backup"
-  duplicity --progress --progress-rate 5 --no-encryption --allow-source-mismatch --full-if-older-than 7D $PATH_BACKUP $S3_URL
+  duplicity --progress --progress-rate 5 --no-encryption --allow-source-mismatch --full-if-older-than 7D $PATH_TO_BACKUP $S3_URL
   echo "Starting cleanup"
   duplicity --progress --progress-rate 5 remove-all-but-n-full 3 --force --no-encryption --allow-source-mismatch $S3_URL
   duplicity --progress --progress-rate 5 cleanup --force --no-encryption $S3_URL
